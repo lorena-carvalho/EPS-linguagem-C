c0f44d7cc58dd24741cff316130df7b426e80957
Lorena Carvalho <78042293+lorena-carvalho@users.noreply.github.com>
2022-05-16 14:59:32 +0000

    Create ep_1.1.c

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # EPS Llinguagem C
+
+## Descrição
+Exercícios de programação estruturada em linguagem C
+
+ep 1.0 - 
+´´´
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
+´´´
---
+++ eps/ep_1.1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int  main(void){
+  int n,i;
+  float numero, soma=0, menor = 999, maior = -2, media;
+
+  scanf("%d",&n);
+
+  for(i=0;i<n;i++){
+    scanf("%float", &numero);
+    soma = soma + numero;
+
+    if (numero > maior){
+      maior = numero;
+    }
+    if (numero < menor){
+      menor = numero;
+    }   
+  }
+
+  media = soma/n;
+
+  printf("Menor: %.2f\n",menor);
+  printf("Maior: %.2f\n",maior);
+  printf("Média: %.2f\n",media);
+  
+  return 0;
+}
